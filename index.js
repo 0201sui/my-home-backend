c772ada07d63c996eb7c8c5b8c2caadebeccbabd
0201sui <3403067540@qq.com>
2026-07-08 08:44:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.post('/chat', async (req, res) => {
     }
 
     try {
-        const response = await fetch('https://xn--vduyey89e.com/v1/chat/completions', {
+        const response = await fetch('https://xn--vduyey89e.com/v1', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
